f37d81e15688ea0734058806014f4ad69b7d6a91
Kilian Pérez González <kilianpg@gmail.com>
2018-03-03 03:04:45 +0100

    Implementación de la lectura del fichero maestro utilizando la biblioteca OpenCSV (issue #8).

--- src/main/java/asw/agents/util/Utilidades.java
+++ src/main/java/asw/agents/util/Utilidades.java
@@ -3,6 +3,12 @@ package asw.agents.util;
 import java.util.Calendar;
 import java.util.Date;
 
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
+import com.opencsv.CSVReader;
 
 public  class Utilidades {
 	///////////////////////////////////////////////////////////////
@@ -47,15 +53,28 @@ public  class Utilidades {
 //
 //	}
 
-    public static int getKindCode(String kind) {
-
-		if(kind.equals("Person"))
-			return 1;
-		if(kind.equals("Entity"))
-			return 2;
-		if(kind.equals("Sensor"))
-			return 3;
-
-		return -1; // si no existe
-	}
+	public static int getKindCode(String kind) {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		Properties properties = new Properties();
+		try(InputStream resourceStream = classLoader.getResourceAsStream("application.properties")) {
+		    properties.load(resourceStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        CSVReader csvReader = null;
+        try {
+        	csvReader = new CSVReader(new InputStreamReader(
+        			classLoader.getResourceAsStream((properties.getProperty("csv.filepathname"))))
+        	);
+            String[] nextRow;
+            while ((nextRow = csvReader.readNext()) != null) {
+            	if(kind.equals(nextRow[1]))
+        			return Integer.parseInt(nextRow[0]);
+            }
+            csvReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }        
+        return -1;
+	}	
 }
