707e5a6bd3b5ac413db0b76feb370d946b3e6181
Kilian Pérez González <kilianpg@gmail.com>
2018-03-30 02:27:28 +0200

    Parametriza la configuración del fichero maestro CSV usando anotaciones

--- src/main/java/asw/agents/util/Utilidades.java
+++ src/main/java/asw/agents/util/Utilidades.java
@@ -1,35 +1,50 @@
 package asw.agents.util;
 
-import java.util.Properties;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
 import com.opencsv.CSVReader;
 
-public  class Utilidades {
 
-	public static int getKindCode(String kind) {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		Properties properties = new Properties();
-		try(InputStream resourceStream = classLoader.getResourceAsStream("application.properties")) {
-		    properties.load(resourceStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        CSVReader csvReader = null;
-        try {
-        	csvReader = new CSVReader(new InputStreamReader(
-        			classLoader.getResourceAsStream((properties.getProperty("csv.filepathname"))))
-        	);
+@Component
+public class Utilidades {
+	private static final Logger logger = Logger.getLogger(Utilidades.class);
+
+	static String csv_filepathname;
+	
+	// Hack to inject value into static variable
+	// (yes, updating static non-final field from 
+	// non-static method is asking for a problem...):
+	@Value("${csv.filepathname:tipo_agentes.csv}") 
+	public void set_csv_filepathname (String filepathname) {
+		csv_filepathname = filepathname;
+	}
+	
+	@Autowired
+	public static int getKindCode(String kind) {	
+		CSVReader csvReader = null;				
+		logger.info("Search kindCode for kind '" + kind + "' on file '" + csv_filepathname + "'");				
+        try {        	
+        	csvReader = new CSVReader(new FileReader (new ClassPathResource(csv_filepathname).getFile()));        	
             String[] nextRow;
             while ((nextRow = csvReader.readNext()) != null) {
-            	if(kind.equals(nextRow[1]))
+            	if(kind.equals(nextRow[1])) {
+            		csvReader.close();
+            		logger.info("Success on finding kindCode '" + nextRow[0] + "' for kind '" + kind + "' on file '" + csv_filepathname + "'");            		
         			return Integer.parseInt(nextRow[0]);
+            	}
             }
             csvReader.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }        
+        }         
+        logger.error("There is not any kindCode for kind '" + kind + "' on file '" + csv_filepathname + "'");
         return -1;
 	}	
 }
